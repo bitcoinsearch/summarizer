4c8b9a42732b0fa399c44e55a77d9d435fd8b8e5
urvish <urvishp80@gmail.com>
2023-07-18 18:38:01 -0400

    added homepage XML file to populate homepage, added cron job for it

---
+++ generate_homepage_xml.py
@@ -0,0 +1,460 @@
+import re
+import pandas as pd
+from feedgen.feed import FeedGenerator
+from elasticsearch import Elasticsearch
+import time
+import traceback
+import openai
+from datetime import datetime, timedelta
+from loguru import logger
+import xml.etree.ElementTree as ET
+import os
+from dotenv import load_dotenv
+import sys
+import warnings
+import pytz
+
+from src.gpt_utils import generate_chatgpt_summary, consolidate_chatgpt_summary
+from src.config import TOKENIZER, ES_CLOUD_ID, ES_USERNAME, ES_PASSWORD, ES_INDEX, ES_DATA_FETCH_SIZE
+
+import numpy as np
+
+warnings.filterwarnings("ignore")
+load_dotenv()
+
+# if set to True, it will use chatgpt model ("gpt-3.5-turbo") for all the completions
+CHATGPT = True
+
+# COMPLETION_MODEL - only applicable if CHATGPT is set to False
+OPENAI_ORG_KEY = os.getenv("OPENAI_ORG_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+openai.organization = OPENAI_ORG_KEY
+openai.api_key = OPENAI_API_KEY
+
+
+class ElasticSearchClient:
+    def __init__(self, es_cloud_id, es_username, es_password, es_data_fetch_size=ES_DATA_FETCH_SIZE) -> None:
+        self._es_cloud_id = es_cloud_id
+        self._es_username = es_username
+        self._es_password = es_password
+        self._es_data_fetch_size = es_data_fetch_size
+        self._es_client = Elasticsearch(
+            cloud_id=self._es_cloud_id,
+            http_auth=(self._es_username, self._es_password),
+        )
+
+    def extract_data_from_es(self, es_index, url, start_date_str, current_date_str):
+        output_list = []
+        start_time = time.time()
+
+        if self._es_client.ping():
+            logger.info("connected to the ElasticSearch")
+            query = {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "prefix": {  # Using prefix query for domain matching
+                                    "domain.keyword": str(url)
+                                }
+                            },
+                            {
+                                "range": {
+                                    "created_at": {
+                                        "gte": f"{start_date_str}T00:00:00.000Z",
+                                        "lte": f"{current_date_str}T23:59:59.999Z"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+
+            # Initialize the scroll
+            scroll_response = self._es_client.search(index=es_index, body=query, size=self._es_data_fetch_size,
+                                                     scroll='1m')
+            scroll_id = scroll_response['_scroll_id']
+            results = scroll_response['hits']['hits']
+
+            # Dump the documents into the json file
+            logger.info(f"Starting dumping of {es_index} data in json...")
+            # output_data_path = f'{data_path}/{es_index}.json'
+            # with open(output_data_path, 'w') as f:
+            while len(results) > 0:
+                # Save the current batch of results
+                for result in results:
+                    output_list.append(result)
+
+                # Fetch the next batch of results
+                scroll_response = self._es_client.scroll(scroll_id=scroll_id, scroll='1m')
+                scroll_id = scroll_response['_scroll_id']
+                results = scroll_response['hits']['hits']
+
+            logger.info(
+                f"Dumping of {es_index} data in json has completed and has taken {time.time() - start_time:.2f} seconds.")
+
+            return output_list
+        else:
+            logger.info('Could not connect to Elasticsearch')
+            return None
+
+    def filter_top_recent_posts(self, es_results, top_n):
+        es_results_sorted = sorted(
+            es_results,
+            key=lambda x: datetime.strptime(x['_source']['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ'), reverse=True
+        )
+        return es_results_sorted[:top_n]
+
+    def fetch_all_data_for_url(self, es_index):
+        logger.info(f"fetching all the data")
+        output_list = []
+        start_time = time.time()
+
+        if self._es_client.ping():
+            logger.info("connected to the ElasticSearch")
+            query = {}
+
+            # Initialize the scroll
+            scroll_response = self._es_client.search(index=es_index, body=query, size=self._es_data_fetch_size,
+                                                     scroll='1m')
+            scroll_id = scroll_response['_scroll_id']
+            results = scroll_response['hits']['hits']
+
+            # Dump the documents into the json file
+            logger.info(f"Starting dumping of {es_index} data in json...")
+            # output_data_path = f'{data_path}/{es_index}.json'
+            # with open(output_data_path, 'w') as f:
+            while len(results) > 0:
+                # Save the current batch of results
+                for result in results:
+                    output_list.append(result['_source'])
+
+                # Fetch the next batch of results
+                scroll_response = self._es_client.scroll(scroll_id=scroll_id, scroll='1m')
+                scroll_id = scroll_response['_scroll_id']
+                results = scroll_response['hits']['hits']
+
+            logger.info(
+                f"Dumping of {es_index} data in json has completed and has taken {time.time() - start_time:.2f} seconds.")
+
+            df = pd.DataFrame(output_list)
+            logger.info(f"Total threads received for: {df.shape[0]}")
+            return df
+        else:
+            logger.info('Could not connect to Elasticsearch')
+            return None
+
+    def fetch_contributors_and_threads(self, title, domain, df):
+        df_filtered = df.loc[(df['title'] == title) & (df['domain'] == domain)]
+        counts = len(df_filtered)
+        df_filtered['authors'] = df_filtered['authors'].apply(tuple)
+        contributors = df_filtered['authors'].tolist()
+        contributors = [i[0] for i in contributors]
+        contributors = list(np.unique(contributors))
+        return counts, contributors
+
+
+class GenerateXML:
+    def __init__(self) -> None:
+        self.month_dict = {
+            1: "Jan", 2: "Feb", 3: "March", 4: "April", 5: "May", 6: "June",
+            7: "July", 8: "Aug", 9: "Sept", 10: "Oct", 11: "Nov", 12: "Dec"
+        }
+
+    def split_prompt_into_chunks(self, prompt, chunk_size):
+        tokens = TOKENIZER.encode(prompt)
+        chunks = []
+
+        while len(tokens) > 0:
+            current_chunk = TOKENIZER.decode(tokens[:chunk_size]).strip()
+
+            if current_chunk:
+                chunks.append(current_chunk)
+
+            tokens = tokens[chunk_size:]
+
+        return chunks
+
+    def get_summary_chunks(self, body, tokens_per_sub_body):
+        chunks = self.split_prompt_into_chunks(body, tokens_per_sub_body)
+        summaries = []
+
+        print(f"Total chunks: {len(chunks)}")
+
+        for chunk in chunks:
+            count = 0
+            while True:
+                try:
+                    time.sleep(2)
+                    summary = generate_chatgpt_summary(chunk)
+                    summaries.append(summary)
+                    break
+                except Exception as ex:
+                    count += 1
+                    if count > 5:
+                        sys.exit(f"Chunk summary ran into error: {traceback.format_exc()}")
+
+        return summaries
+
+    def recursive_summary(self, body, tokens_per_sub_body, max_length):
+        summaries = self.get_summary_chunks(body, tokens_per_sub_body)
+
+        summary_length = sum([len(TOKENIZER.encode(s)) for s in summaries])
+
+        print(f"Summary length: {summary_length}")
+        print(f"Max length: {max_length}")
+
+        if summary_length > max_length:
+            print("entering in recursion")
+            return self.recursive_summary("".join(summaries), tokens_per_sub_body, max_length)
+        else:
+            return summaries
+
+    def gpt_api(self, body):
+        body_length_limit = 2800
+        tokens_per_sub_body = 2700
+        summaries = self.recursive_summary(body, tokens_per_sub_body, body_length_limit)
+
+        if len(summaries) > 1:
+            print("Consolidate summary generating")
+            summary_str = "\n".join(summaries)
+            count = 0
+            while True:
+                try:
+                    time.sleep(2)
+                    consolidated_summaries = consolidate_chatgpt_summary(summary_str)
+                    break
+                except Exception as ex:
+                    count += 1
+                    if count > 5:
+                        sys.exit(f"Chunk summary ran into error: {traceback.format_exc()}")
+
+            return consolidated_summaries
+
+        else:
+            print("Individual summary generating")
+            return "\n".join(summaries)
+
+    def create_summary(self, body):
+        summ = self.gpt_api(body)
+        return summ
+
+    def clean_title(self, xml_name):
+        special_characters = ['/', ':', '@', '#', '$', '*', '&', '<', '>', '\\', '?']
+        xml_name = re.sub(r'[^A-Za-z0-9]+', '-', xml_name)
+        for sc in special_characters:
+            xml_name = xml_name.replace(sc, "-")
+        return xml_name
+
+    def get_id(self, id):
+        return str(id).split("-")[-1]
+
+    def create_n_bullets(self, body_summary, n=3):
+        bullets_prompt = f"Generate {n} bullet points based on below context. \n\n CONTEXT:\n\n{body_summary}"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are an intelligent."},
+                {"role": "user", "content": f"{bullets_prompt}"},
+            ],
+            temperature=0.7,
+            max_tokens=500,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=1
+        )
+        response_str = response['choices'][0]['message']['content'].replace("\n", "").strip()
+        return response_str
+
+    def get_xml_summary(self, data):
+        number = self.get_id(data["_source"]["id"])
+        title = data["_source"]["title"]
+        xml_name = self.clean_title(title)
+        published_at = datetime.strptime(data['_source']['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        published_at = pytz.UTC.localize(published_at)
+        month_name = self.month_dict[int(published_at.month)]
+        str_month_year = f"{month_name}_{int(published_at.year)}"
+        dev_name = data['_source']['dev_name']
+
+        current_directory = os.getcwd()
+        file_path = f"static/{dev_name}/{str_month_year}/{number}_{xml_name}.xml"
+        full_path = os.path.join(current_directory, file_path)
+
+        if os.path.exists(full_path):
+            namespaces = {'atom': 'http://www.w3.org/2005/Atom'}
+            tree = ET.parse(full_path)
+            root = tree.getroot()
+            summ_list = root.findall(".//atom:entry/atom:summary", namespaces)
+            summ = "\n".join([summ.text for summ in summ_list])
+            return summ
+        else:
+            logger.info(f"No xml file found: {full_path}")
+            return None
+
+    def generate_recent_posts_summary(self, dict_list):
+        logger.info("working on recent post's summary")
+
+        recent_post_data = ""
+
+        for data in dict_list:
+            xml_summ = self.get_xml_summary(data)
+            recent_post_data += xml_summ
+
+            if xml_summ is None:
+                body = data['_source']['body']
+                body_summ = self.create_summary(body)
+                recent_post_data += body_summ
+
+        summ_prompt = f"Consolidate below context in 3 sentences strictly. \n\n CONTEXT:\n\n{recent_post_data}"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are an intelligent."},
+                {"role": "user", "content": f"{summ_prompt}"},
+            ],
+            temperature=0.7,
+            max_tokens=500
+        )
+        response_str = response['choices'][0]['message']['content'].replace("\n", "").strip()
+        return response_str
+
+    def create_atom_feed(self, dict_list, file_name="homepage.xml"):
+
+        fg = FeedGenerator()
+        fg.id("1")
+
+        recent_post_summ = self.generate_recent_posts_summary(dict_list)
+        fg.title(recent_post_summ)
+
+        for data in dict_list:
+
+            number = self.get_id(data["_source"]["id"])
+            title = data["_source"]["title"]
+            published_at = datetime.strptime(data['_source']['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
+            published_at = pytz.UTC.localize(published_at)
+            contributors = data['_source']['contributors']
+            url = data['_source']['url']
+            authors = data['_source']['authors']
+            body = data['_source']['body']
+
+            # fetch the summary from xml if exist
+            xml_summary = self.get_xml_summary(data)
+
+            if xml_summary is None:
+                xml_summary = self.create_summary(body)
+
+            bullets = self.create_n_bullets(xml_summary, n=3)
+
+            summary_data = str({
+                "summary": bullets,
+                "n_threads": data["_source"]["n_threads"],
+                "dev_name": data['_source']['dev_name']
+            })
+
+            entry = fg.add_entry()
+            entry.id(number)
+            entry.title(title)
+            entry.link(href=url, rel='alternate')
+            for author in authors:
+                entry.author(name=author)
+            entry.published(published_at)
+            entry.summary(summary_data)
+            for contributor in contributors:
+                entry.contributor(name=contributor)
+
+        atom_string = fg.atom_str(pretty=True)
+
+        f_name = f"static/{file_name}"
+        logger.info(f_name)
+        with open(f_name, 'wb') as f:
+            f.write(atom_string)
+            logger.info(f"saved file: {f_name}")
+        return f_name
+
+    def start_process(self, dict_data):
+        logger.info("start start_process")
+        if len(dict_data) > 0:
+            _ = self.create_atom_feed(dict_data)
+        else:
+            logger.error(f"Data list empty! Please check the data again.")
+
+    def get_existing_xml_ids(self, file_path):
+        current_directory = os.getcwd()
+        full_path = os.path.join(current_directory, file_path)
+        if os.path.exists(full_path):
+            namespaces = {'atom': 'http://www.w3.org/2005/Atom'}
+            tree = ET.parse(file_path)
+            root = tree.getroot()
+            id_list = root.findall(".//atom:entry/atom:id", namespaces)
+            return [id.text for id in id_list]
+        else:
+            logger.info(f"No homepage.xml file found: {full_path}")
+            return []
+
+
+if __name__ == "__main__":
+    gen = GenerateXML()
+    elastic_search = ElasticSearchClient(es_cloud_id=ES_CLOUD_ID, es_username=ES_USERNAME,
+                                         es_password=ES_PASSWORD)
+
+    all_data_df = elastic_search.fetch_all_data_for_url(ES_INDEX)
+
+    dev_urls = [
+        "https://lists.linuxfoundation.org/pipermail/bitcoin-dev/",
+        "https://lists.linuxfoundation.org/pipermail/lightning-dev/"
+    ]
+    current_date_str = None
+    if not current_date_str:
+        current_date_str = datetime.now().strftime("%Y-%m-%d")
+
+    start_date = datetime.now() - timedelta(days=30)
+    start_date_str = start_date.strftime("%Y-%m-%d")
+    logger.info(f"start_date: {start_date_str}")
+    logger.info(f"current_date_str: {current_date_str}")
+
+    recent_data_list = []
+    for dev_url in dev_urls:
+        data_list = elastic_search.extract_data_from_es(ES_INDEX, dev_url, start_date_str, current_date_str)
+        dev_name = dev_url.split("/")[-2]
+        logger.info(f"Total threads received for {dev_name}: {len(data_list)}")
+
+        data_list = elastic_search.filter_top_recent_posts(es_results=data_list, top_n=3)
+        logger.info(f"collected top_n results for {dev_name}")
+
+        for data in data_list:
+            title = data['_source']['title']
+            counts, contributors = elastic_search.fetch_contributors_and_threads(title=title, domain=dev_url,
+                                                                                 df=all_data_df)
+            authors = data['_source']['authors']
+            for author in authors:
+                contributors.remove(author)
+            data['_source']['n_threads'] = counts
+            data['_source']['contributors'] = contributors
+            data['_source']['dev_name'] = dev_name
+            recent_data_list.append(data)
+
+    logger.info(f"Number of recent posts collected: {len(recent_data_list)}")
+
+    xml_ids = gen.get_existing_xml_ids(file_path=r"static/homepage.xml")
+    recent_post_ids = [gen.get_id(data['_source']['id']) for data in recent_data_list]
+
+    if set(recent_post_ids) != set(xml_ids):
+        logger.info("changes found in recent posts")
+
+        delay = 1
+        count = 0
+
+        while True:
+            try:
+                gen.start_process(recent_data_list)
+                break
+            except Exception as ex:
+                logger.error(ex)
+                time.sleep(delay)
+                count += 1
+                if count > 5:
+                    sys.exit(ex)
+    else:
+        logger.info("No change in recent posts, no need to update homepage.xml file")
