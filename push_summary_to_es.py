4f05b778b09eb92c39e4be70ffb5f5b9335d99f0
Urviskumar <urvishp80@gmail.com>
2023-08-04 15:17:24 -0400

    Feat/push summary to es (#21)
    * added script to push summary back to es index and cron job for it
    * added cron job to push summary back to es index
    * updated cron job to push summary back to es index and its script

---
+++ push_summary_to_es.py
@@ -0,0 +1,252 @@
+import re
+from elasticsearch import Elasticsearch
+import time
+import openai
+from datetime import datetime, timedelta
+from loguru import logger
+import xml.etree.ElementTree as ET
+import os
+from dotenv import load_dotenv
+import warnings
+import pytz
+import tqdm
+
+from src.config import TOKENIZER, ES_CLOUD_ID, ES_USERNAME, ES_PASSWORD, ES_INDEX, ES_DATA_FETCH_SIZE
+
+warnings.filterwarnings("ignore")
+load_dotenv()
+
+# if set to True, it will use chatgpt model ("gpt-3.5-turbo") for all the completions
+CHATGPT = True
+
+# COMPLETION_MODEL - only applicable if CHATGPT is set to False
+OPENAI_ORG_KEY = os.getenv("OPENAI_ORG_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+openai.organization = OPENAI_ORG_KEY
+openai.api_key = OPENAI_API_KEY
+
+
+class ElasticSearchClient:
+    def __init__(self, es_cloud_id, es_username, es_password, es_data_fetch_size=ES_DATA_FETCH_SIZE) -> None:
+        self._es_cloud_id = es_cloud_id
+        self._es_username = es_username
+        self._es_password = es_password
+        self._es_data_fetch_size = es_data_fetch_size
+        self._es_client = Elasticsearch(
+            cloud_id=self._es_cloud_id,
+            http_auth=(self._es_username, self._es_password),
+        )
+
+    def fetch_all_data_for_url(self, es_index, url):
+        logger.info(f"fetching all the data")
+        output_list = []
+        start_time = time.time()
+
+        if self._es_client.ping():
+            logger.info("connected to the ElasticSearch")
+            query = {
+                "query": {
+                    "match_phrase": {
+                        "domain": str(url)
+                    }
+                }
+            }
+
+            # Initialize the scroll
+            scroll_response = self._es_client.search(index=es_index, body=query, size=self._es_data_fetch_size,
+                                                     scroll='1m')
+            scroll_id = scroll_response['_scroll_id']
+            results = scroll_response['hits']['hits']
+
+            # Dump the documents into the json file
+            logger.info(f"Starting dumping of {es_index} data in json...")
+            while len(results) > 0:
+                # Save the current batch of results
+                for result in results:
+                    output_list.append(result)
+
+                # Fetch the next batch of results
+                scroll_response = self._es_client.scroll(scroll_id=scroll_id, scroll='1m')
+                scroll_id = scroll_response['_scroll_id']
+                results = scroll_response['hits']['hits']
+
+            logger.info(
+                f"Dumping of {es_index} data in json has completed and has taken {time.time() - start_time:.2f} seconds.")
+
+            return output_list
+        else:
+            logger.info('Could not connect to Elasticsearch')
+            return None
+
+    def extract_data_from_es(self, es_index, url, start_date_str, current_date_str):
+        output_list = []
+        start_time = time.time()
+
+        if self._es_client.ping():
+            logger.success("connected to the ElasticSearch")
+            query = {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "prefix": {  # Using prefix query for domain matching
+                                    "domain.keyword": str(url)
+                                }
+                            },
+                            {
+                                "range": {
+                                    "created_at": {
+                                        "gte": f"{start_date_str}T00:00:00.000Z",
+                                        "lte": f"{current_date_str}T23:59:59.999Z"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+
+            # Initialize the scroll
+            scroll_response = self._es_client.search(
+                index=es_index,
+                body=query,
+                size=self._es_data_fetch_size,
+                scroll='1m'
+            )
+            scroll_id = scroll_response['_scroll_id']
+            results = scroll_response['hits']['hits']
+
+            # Dump the documents into the json file
+            logger.info(f"Starting dumping of {es_index} data in json...")
+            while len(results) > 0:
+                # Save the current batch of results
+                for result in results:
+                    output_list.append(result)
+
+                # Fetch the next batch of results
+                scroll_response = self._es_client.scroll(scroll_id=scroll_id, scroll='1m')
+                scroll_id = scroll_response['_scroll_id']
+                results = scroll_response['hits']['hits']
+
+            logger.info(
+                f"Dumping of {es_index} data in json has completed and has taken {time.time() - start_time:.2f} seconds.")
+
+            return output_list
+        else:
+            logger.info('Could not connect to Elasticsearch')
+            return None
+
+    @property
+    def es_client(self):
+        return self._es_client
+
+
+class XMLReader:
+    def __init__(self) -> None:
+        self.month_dict = {
+            1: "Jan", 2: "Feb", 3: "March", 4: "April", 5: "May", 6: "June",
+            7: "July", 8: "Aug", 9: "Sept", 10: "Oct", 11: "Nov", 12: "Dec"
+        }
+
+    def get_id(self, id):
+        return str(id).split("-")[-1]
+
+    def clean_title(self, xml_name):
+        special_characters = ['/', ':', '@', '#', '$', '*', '&', '<', '>', '\\', '?']
+        xml_name = re.sub(r'[^A-Za-z0-9]+', '-', xml_name)
+        for sc in special_characters:
+            xml_name = xml_name.replace(sc, "-")
+        return xml_name
+
+    def get_xml_summary(self, data, dev_name):
+        number = self.get_id(data["_source"]["id"])
+        title = data["_source"]["title"]
+        xml_name = self.clean_title(title)
+        published_at = datetime.strptime(data['_source']['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        published_at = pytz.UTC.localize(published_at)
+        month_name = self.month_dict[int(published_at.month)]
+        str_month_year = f"{month_name}_{int(published_at.year)}"
+        current_directory = os.getcwd()
+        file_path = f"static/{dev_name}/{str_month_year}/{number}_{xml_name}.xml"
+        full_path = os.path.join(current_directory, file_path)
+
+        try:
+            if os.path.exists(full_path):
+                namespaces = {'atom': 'http://www.w3.org/2005/Atom'}
+                tree = ET.parse(full_path)
+                root = tree.getroot()
+                summ_list = root.findall(".//atom:entry/atom:summary", namespaces)
+                if summ_list:
+                    summ = "\n".join([summ.text for summ in summ_list])
+                    return summ, f"Summary text found: {full_path}"
+                else:
+                    return None, f"No summary found: {full_path}"
+            else:
+                return None, f"No xml file found: {full_path}"
+        except Exception as e:
+            ex_message = f"Error: {e} at file path: {full_path}"
+            logger.error(ex_message)
+            return None, ex_message
+
+
+if __name__ == "__main__":
+
+    # if production is set to False, elasticsearch will fetch all the docs in the index
+    PRODUCTION = True
+
+    # workflow
+    xml_reader = XMLReader()
+    elastic_search = ElasticSearchClient(es_cloud_id=ES_CLOUD_ID, es_username=ES_USERNAME,
+                                         es_password=ES_PASSWORD)
+
+    dev_urls = [
+        "https://lists.linuxfoundation.org/pipermail/bitcoin-dev/",
+        "https://lists.linuxfoundation.org/pipermail/lightning-dev/"
+    ]
+
+    for dev_url in dev_urls:
+        logger.info(f"dev_url: {dev_url}")
+
+        if PRODUCTION:
+            current_date_str = None
+            if not current_date_str:
+                current_date_str = datetime.now().strftime("%Y-%m-%d")
+            start_date = datetime.now() - timedelta(days=15)
+            start_date_str = start_date.strftime("%Y-%m-%d")
+            logger.info(f"start_date: {start_date_str}")
+            logger.info(f"current_date_str: {current_date_str}")
+
+            docs_list = elastic_search.extract_data_from_es(ES_INDEX, dev_url, start_date_str, current_date_str)
+
+        else:
+            docs_list = elastic_search.fetch_all_data_for_url(ES_INDEX, dev_url)
+
+        dev_name = dev_url.split("/")[-2]
+        logger.success(f"Total threads received for {dev_name}: {len(docs_list)}")
+
+        for doc in tqdm.tqdm(docs_list):
+            res = None
+            try:
+                doc_id = doc['_id']
+                doc_index = doc['_index']
+                if not doc['_source'].get('summary'):
+                    xml_summary, res = xml_reader.get_xml_summary(doc, dev_name)
+
+                    if xml_summary:
+                        elastic_search.es_client.update(
+                            index=doc_index,
+                            id=doc_id,
+                            body={
+                                'doc': {
+                                    "summary": xml_summary
+                                }
+                            }
+                        )
+            except Exception as ex:
+                error_message = f"Error occurred: {ex}"
+                if res:
+                    error_message += f", Response: {res}"
+                logger.error(error_message)
+
+    logger.success(f"Process complete.")
