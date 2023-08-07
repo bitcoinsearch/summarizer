45693fde0bc0ef1c15b1fa94fcacaf28d6ba61c6
urvish <urvishp80@gmail.com>
2023-08-07 19:15:21 -0400

    added remainder of the summaries, added line breaks in prompt

--- src/gpt_utils.py
+++ src/gpt_utils.py
@@ -54,6 +54,7 @@ def generate_chatgpt_summary(prompt):
     summarization_prompt = f"Generate a detailed summary from below context fact fully without missing any important " \
                            f"information. Try to retain all the links provided and use them in proper manner at " \
                            f"proper place. Do not use the word 'summary' in it. Make the summary in paragraphs and " \
+                           f"Use line breaks like `\n` in between new paragraphs. Make this a must to do when you create more paragraphs." \
                            f"use line breaks at proper places. Use cohesive writing style. \n\n CONTEXT:\n\n{prompt}"
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -75,7 +76,8 @@ def consolidate_chatgpt_summary(prompt):
     consolidate_prompt = f"""Consolidate below context based on the guidelines mentioned below. 
     1. Please adhere to all English grammatical rules while writing the sentences, maintaining formal tone and employing proper spacing. 
     2. While summarizing, avoid using phrases referring to the context. Instead, directly present the information or points covered. 
-    Do not introduce sentences with phrases like: "The context discusses...", "In this context..." or "The context covers..."
+    Do not introduce sentences with phrases like: "The context discusses...", "In this context..." or "The context covers...".
+    3. Use line breaks like "\n" in between new paragraphs. Make this a must to do when you create more paragraphs.
     \n\n CONTEXT:\n\n{prompt}"""
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
