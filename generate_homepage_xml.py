d8b2f544927563f467b16ca5360cea9a8ac8985b
urvishp80 <urvishp80@gmail.com>
2023-12-15 05:37:25 -0500

    today in history snippets in homepage.json file

--- generate_homepage_xml.py
+++ generate_homepage_xml.py
@@ -1,3 +1,4 @@
+import random
 import re
 import pandas as pd
 from elasticsearch import Elasticsearch
@@ -15,6 +16,7 @@ import pytz
 import json
 import numpy as np
 import nltk
+import shutil
 
 nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
@@ -434,7 +436,7 @@ class GenerateJSON:
         return response_str
 
     def create_single_entry(self, data, base_url_for_xml="static", look_for_combined_summary=False,
-                            remove_xml_extension=False):
+                            remove_xml_extension=False, add_combined_summary_field=False):
         number = self.get_id(data["_source"]["id"])
         title = data["_source"]["title"]
         published_at = datetime.strptime(data['_source']['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -447,12 +449,16 @@ class GenerateJSON:
         xml_name = self.clean_title(title)
         month_name = self.month_dict[int(published_at.month)]
         str_month_year = f"{month_name}_{int(published_at.year)}"
+        current_directory = os.getcwd()
 
         combined_summ_file_path = ""
         base_path = f"{base_url_for_xml}/{local_dev_name}/{str_month_year}"
         file_extension = "" if remove_xml_extension else ".xml"
         if look_for_combined_summary:
-            combined_summ_file_path = f"{base_path}/combined_{xml_name}{file_extension}"
+            if os.path.exists(f"static/{local_dev_name}/{str_month_year}/combined_{xml_name}.xml"):
+                combined_summ_file_path = f"{base_path}/combined_{xml_name}{file_extension}"
+            else:
+                combined_summ_file_path = ""
         file_path = f"{base_path}/{number}_{xml_name}{file_extension}"
 
         # fetch the summary from xml if exist
@@ -474,44 +480,24 @@ class GenerateJSON:
             "dev_name": local_dev_name,
             "contributors": contributors,
             "file_path": file_path,
-            "combined_summ_file_path": combined_summ_file_path if os.path.exists(
-                f"static/{local_dev_name}/{str_month_year}/combined_{xml_name}.xml") else ""
+            "combined_summ_file_path": combined_summ_file_path
         }
+
+        combined_summ_bullets = ""
+        if add_combined_summary_field:
+            combined_summ_full_path = os.path.join(current_directory, combined_summ_file_path)
+
+            if os.path.exists(combined_summ_full_path):
+                namespaces = {'atom': 'http://www.w3.org/2005/Atom'}
+                tree = ET.parse(combined_summ_full_path)
+                root = tree.getroot()
+                summ_list = root.findall(".//atom:entry/atom:summary", namespaces)
+                combined_summ = "\n".join([summ.text for summ in summ_list])
+                combined_summ_bullets = self.create_n_bullets(combined_summ, n=3)
+
+            entry_data['combined_summary'] = combined_summ_bullets if combined_summ_bullets else ""
+
         return entry_data
-
-    def create_json_feed(self, recent_dict_list, active_data_list, file_name="homepage.json"):
-        recent_post_summ = self.generate_recent_posts_summary(recent_dict_list)
-
-        logger.success(recent_post_summ)
-
-        json_string = {"header_summary": recent_post_summ}
-
-        recent_page_data = []
-        for data in recent_dict_list:
-            entry_data = self.create_single_entry(data)
-            recent_page_data.append(entry_data)
-
-        json_string["recent_posts"] = recent_page_data
-
-        active_page_data = []
-        for data in active_data_list:
-            entry_data = self.create_single_entry(data, look_for_combined_summary=True)
-            active_page_data.append(entry_data)
-
-        json_string["active_posts"] = active_page_data
-
-        f_name = f"static/{file_name}"
-        with open(f_name, 'w') as f:
-            f.write(json.dumps(json_string, indent=4))
-            logger.success(f"saved file: {f_name}")
-        return f_name
-
-    def start_process(self, data_list_1, data_list_2):
-        logger.info("Creating homepage.json file ... ")
-        if len(data_list_1) > 0 or len(data_list_2) > 0:
-            _ = self.create_json_feed(data_list_1, data_list_2)
-        else:
-            logger.error(f"Data list empty! Please check the data again.")
 
     def get_existing_json_ids(self, file_path):
         current_directory = os.getcwd()
@@ -543,18 +529,30 @@ if __name__ == "__main__":
         "https://lists.linuxfoundation.org/pipermail/bitcoin-dev/",
         "https://lists.linuxfoundation.org/pipermail/lightning-dev/"
     ]
-    current_date_str = None
-    if not current_date_str:
-        current_date_str = datetime.now().strftime("%Y-%m-%d")
+
+    current_date = datetime.now()
+    current_date_str = current_date.strftime("%Y-%m-%d")
 
     start_date = datetime.now() - timedelta(days=7)
     start_date_str = start_date.strftime("%Y-%m-%d")
     logger.info(f"start_date: {start_date_str}")
     logger.info(f"current_date_str: {current_date_str}")
 
+    month_name = gen.month_dict[int(current_date.month)]
+    str_month_year = f"{month_name}_{int(current_date.year)}"
+
+    json_file_path = fr"static/homepage.json"
+
     recent_data_list = []
     active_data_list = []
+    today_in_history_data_list = []  # Posts that were created on today's date on previous years
+
+    random_years_ago = None
+
     for dev_url in dev_urls:
+        logger.info(f"Working on URL: {dev_url}")
+        fetch_today_in_history = True
+
         all_data_df, all_data_list = elastic_search.fetch_all_data_for_url(ES_INDEX, url=dev_url)
         data_list = elastic_search.extract_data_from_es(ES_INDEX, dev_url, start_date_str, current_date_str)
         dev_name = dev_url.split("/")[-2]
@@ -576,12 +574,13 @@ if __name__ == "__main__":
                 continue
             seen_titles.add(title)
 
-            counts, contributors = elastic_search.fetch_contributors_and_threads(title=title, domain=dev_url,
-                                                                                 df=all_data_df)
             # get the first post's info of this title
             df_title = all_data_df.loc[(all_data_df['title'] == title) & (all_data_df['domain'] == dev_url)]
             df_title.sort_values(by='created_at', inplace=True)
             original_post = df_title.iloc[0].to_dict()
+
+            counts, contributors = elastic_search.fetch_contributors_and_threads(title=title, domain=dev_url,
+                                                                                 df=df_title)
 
             for i in all_data_list:
                 if i['_source']['title'] == original_post['title'] and i['_source']['domain'] == original_post[
@@ -596,15 +595,14 @@ if __name__ == "__main__":
                     active_posts_data_counter += 1
                     break
 
-        logger.info(f"Number of active posts collected: {len(active_data_list)}")
+        logger.success(f"Number of active posts collected: {len(active_data_list)}")
 
         # top recent posts
         recent_data_post_counter = 0
         recent_posts_data = elastic_search.filter_top_recent_posts(es_results=data_list, top_n=20)
-        # if len(recent_posts_data) >= 3:
-        #     recent_posts_data = recent_posts_data[:3]
 
         for data in recent_posts_data:
+
             # if preprocess body text not longer than token_threshold, skip that post
             if not gen.is_body_text_long(data=data, sent_threshold=2):
                 logger.info(f"skipping: {data['_source']['title']} - {data['_source']['url']}")
@@ -653,9 +651,81 @@ if __name__ == "__main__":
                 recent_data_list.append(data)
                 recent_data_post_counter += 1
 
-        logger.info(f"Number of recent posts collected: {len(recent_data_list)}")
+        logger.success(f"Number of recent posts collected: {len(recent_data_list)}")
 
-    xml_ids = gen.get_existing_json_ids(file_path=r"static/homepage.json")
+        # today in history posts
+        logger.info(f"fetching 'Today in history' posts... ")
+
+        if not random_years_ago:
+            at_least_years_ago = 3
+            at_max_years_ago = current_date.year - 2015
+            random_years_ago = random.randint(at_least_years_ago, at_max_years_ago)
+            logger.info(f"random years ago between {at_least_years_ago} to {at_max_years_ago}: {random_years_ago}")
+
+        default_days_to_look_back = 6
+        loop_counter = 1
+        docs_counter = 0
+
+        while fetch_today_in_history:
+            days_to_look_back = default_days_to_look_back * loop_counter
+            selected_random_date = current_date - timedelta(days=365 * random_years_ago)
+            start_of_time = selected_random_date - timedelta(days=selected_random_date.weekday())
+            end_of_time = start_of_time + timedelta(days=days_to_look_back)
+            logger.info(f"collecting the data for {days_to_look_back} days ... ::: Start of week: {start_of_time}, End of week: {end_of_time}")
+
+            all_data_df['created_at'] = pd.to_datetime(all_data_df['created_at'], errors='coerce')
+            all_data_df['created_at_'] = all_data_df['created_at'].dt.tz_convert(None)
+            df_selected_threads = all_data_df.loc[(all_data_df['created_at_'] >= start_of_time) &
+                                                  (all_data_df['created_at_'] <= end_of_time)]
+            logger.info(f"Shape of df_selected_threads: {df_selected_threads.shape}")
+
+            if len(df_selected_threads) > 0:
+                for idx, row in df_selected_threads.iterrows():
+                    this_title = row['title']
+                    this_created_at = row['created_at'].to_pydatetime().replace(tzinfo=None)
+                    this_type = row['type']
+
+                    if this_type == 'original_post':
+                        logger.info(f"collecting an original thread created at {this_created_at}")
+                        df_title = all_data_df.loc[
+                            (all_data_df['title'] == this_title) & (all_data_df['domain'] == dev_url)]
+                        df_title.sort_values(by='created_at', inplace=True)
+                        logger.info(f"No. of posts found for title: '{this_title}' ::: {df_title.shape}")
+
+                        counts, contributors = elastic_search.fetch_contributors_and_threads(title=this_title,
+                                                                                             domain=dev_url,
+                                                                                             df=df_title)
+                        if counts < 5:
+                            logger.info(f"No. of replies are less than 5, skipping it... ")
+                            continue
+
+                        for d in all_data_list:
+                            if d['_source']['title'] == this_title and d['_source']['domain'] == row['domain'] and \
+                                    d['_source']['authors'] == row['authors'] and d['_source']['url'] == row['url']:
+                                if contributors:
+                                    for author in d['_source']['authors']:
+                                        contributors.remove(author)
+                                d['_source']['n_threads'] = counts
+                                d['_source']['contributors'] = contributors
+                                d['_source']['dev_name'] = dev_name
+                                today_in_history_data_list.append(d)
+                                fetch_today_in_history = False
+                                break
+            loop_counter += 1
+
+        # add history data from yesterday's homepage.json
+        if not today_in_history_data_list:
+            logger.info("Collecting yesterday's history threads!")
+            current_directory = os.getcwd()
+            full_path = os.path.join(current_directory, json_file_path)
+            if os.path.exists(full_path):
+                with open(full_path, 'r') as j:
+                    data = json.load(j)
+                    today_in_history_data_list.extend(data['today_in_history_posts'])
+
+        logger.success(f"No. of 'Today in history' posts collected: {len(today_in_history_data_list)}")
+
+    xml_ids = gen.get_existing_json_ids(file_path=json_file_path)
     recent_post_ids = [gen.get_id(data['_source']['title']) for data in recent_data_list]
     active_post_ids = [gen.get_id(data['_source']['title']) for data in active_data_list]
 
@@ -665,19 +735,71 @@ if __name__ == "__main__":
     if all_post_titles != set(xml_ids):
         logger.info("changes found in recent posts ... ")
 
-        delay = 1
+        delay = 5
         count = 0
 
         while True:
             try:
-                logger.info(f"active posts: {len(active_data_list)}, recent posts: {len(recent_data_list)}")
-                gen.start_process(recent_data_list, active_data_list)
+                logger.info(
+                    f"active posts: {len(active_data_list)}, "
+                    f"recent posts: {len(recent_data_list)}, "
+                    f"today in history posts: {len(today_in_history_data_list)}"
+                )
+                logger.info("Creating homepage.json file ... ")
+
+                if len(active_data_list) > 0 or len(recent_data_list) > 0:
+                    recent_post_summ = gen.generate_recent_posts_summary(recent_data_list)
+                    logger.success(recent_post_summ)
+
+                    # recent data
+                    recent_page_data = []
+                    for data in recent_data_list:
+                        entry_data = gen.create_single_entry(data, look_for_combined_summary=True)
+                        recent_page_data.append(entry_data)
+
+                    # active data
+                    active_page_data = []
+                    for data in active_data_list:
+                        entry_data = gen.create_single_entry(data, look_for_combined_summary=True)
+                        active_page_data.append(entry_data)
+
+                    # today in history
+                    today_in_history_data = []
+                    collected_dev_data = []
+                    if len(today_in_history_data_list) > 0:
+                        for data in today_in_history_data_list:
+                            entry_data = gen.create_single_entry(data, look_for_combined_summary=True,
+                                                                 add_combined_summary_field=True)
+                            if entry_data['dev_name'] not in collected_dev_data:
+                                collected_dev_data.append(entry_data['dev_name'])
+                                logger.info(f"collected data for: {collected_dev_data}")
+                                today_in_history_data.append(entry_data)
+
+                    json_string = {
+                        "header_summary": recent_post_summ,
+                        "recent_posts": recent_page_data,
+                        "active_posts": active_page_data,
+                        "today_in_history_posts": today_in_history_data
+                    }
+
+                    with open(json_file_path, 'w') as f:
+                        f.write(json.dumps(json_string, indent=4))
+                        logger.success(f"saved file: {json_file_path}")
+
+                    archive_json_file_path = fr"static/homepage/{str_month_year}/{current_date_str}-homepage.json"
+                    archive_dirname = os.path.dirname(archive_json_file_path)
+                    os.makedirs(archive_dirname, exist_ok=True)
+                    shutil.copy(json_file_path, archive_json_file_path)
+                    logger.success(f'archive updated with file: {archive_json_file_path}')
+                else:
+                    logger.error(f"Data list empty! Please check the data again.")
                 break
+
             except Exception as ex:
                 logger.error(f"Error occurred: {ex} \n{traceback.format_exc()}")
                 time.sleep(delay)
                 count += 1
                 if count >= 3:
-                    sys.exit(ex)
+                    sys.exit(f"{ex}")
     else:
         logger.success("No change in recent posts, no need to update homepage.json file")
